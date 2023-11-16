6a223eca4fed1b411bf3ad1287c5ae76072f249a
Oleg Kizeev <kizeev.ov@gmail.com>
2023-11-16 14:08:48 +0700

    Fixed qodana warning

--- src/main/kotlin/dev/slint/ideaplugin/lang/SlintLanguage.kt
+++ src/main/kotlin/dev/slint/ideaplugin/lang/SlintLanguage.kt
@@ -1,9 +1,10 @@
 package dev.slint.ideaplugin.lang
 
-import com.intellij.json.JsonLanguage
 import com.intellij.lang.Language
 
 object SlintLanguage : Language("Slint") {
+    private fun readResolve(): Any = SlintLanguage
+
     val INSTANCE = SlintLanguage
 
     override fun getDisplayName(): String = "Slint"
