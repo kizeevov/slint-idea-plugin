586e2c06ace2e0c03834137b75d77c6b65eea4ce
Oleg Kizeev <kizeev.ov@gmail.com>
2024-05-25 00:17:15 +0700

    Refactoring

--- src/main/kotlin/dev/slint/ideaplugin/ide/lsp/SlintLspServerDescriptor.kt
+++ src/main/kotlin/dev/slint/ideaplugin/ide/lsp/SlintLspServerDescriptor.kt
@@ -1,14 +1,11 @@
 package dev.slint.ideaplugin.ide.lsp
 
 import com.intellij.execution.configurations.GeneralCommandLine
-import com.intellij.execution.process.OSProcessHandler
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.platform.lsp.api.Lsp4jClient
-import com.intellij.platform.lsp.api.LspServerListener
-import com.intellij.platform.lsp.api.LspServerNotificationsHandler
-import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
+import com.intellij.platform.lsp.api.*
 import com.intellij.platform.lsp.api.customization.LspCompletionSupport
+import com.intellij.platform.lsp.api.customization.LspFormattingSupport
 import dev.slint.ideaplugin.ide.settings.SlintSettingsState
 import dev.slint.ideaplugin.lang.SlintFileType
 import org.eclipse.lsp4j.services.LanguageServer
@@ -22,13 +19,10 @@ class SlintLspServerDescriptor(project: Project) : ProjectWideLspServerDescripto
 
     override fun createInitializationOptions(): Any = SlintSettingsState.getInstance().lspSettings
 
-    // Suppression of the verifier plugin warning
-    // override fun createLsp4jClient(handler: LspServerNotificationsHandler): Lsp4jClient = LspLanguageClient(handler)
+    override fun createLsp4jClient(handler: LspServerNotificationsHandler): Lsp4jClient = SlintLspClient(handler)
 
-    override fun startServerProcess(): OSProcessHandler =
-        ServerProcessHandler.addListeners(super.startServerProcess(), project)
-
-    override val lsp4jServerClass: Class<out LanguageServer> = SlintLanguageServer::class.java
+    override val lsp4jServerClass: Class<out LanguageServer> = SlintLspServer::class.java
     override val lspServerListener: LspServerListener = SlintLspServerListener(project)
     override val lspCompletionSupport: LspCompletionSupport = SlintLspCompletionSupport()
+    override val lspFormattingSupport: LspFormattingSupport = SlintLspFormattingSupport()
 }
